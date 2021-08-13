95d62b4f4e2290ccbd63538e893fdebd887be3c9
cxxyao2 <cxxyao2@gmail.com>
2021-08-12 22:22:40 -0400

    add ngrx-entity

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
-*  @ngrx/store : a state management tool. It is equivalent to Redux in React.
+- @ngrx/store : a state management tool. It is equivalent to Redux in React.
 
-*  npm i @ngrx/store --save
+- npm i @ngrx/store --save
+
+# build a CRUD application using @ngrx/store
--- src/app/components/article.component.ts
+++ src/app/components/article.component.ts
@@ -1,12 +1,11 @@
 import { Store } from '@ngrx/store';
 
 import { Component, OnInit } from '@angular/core';
-import { from, Observable } from 'rxjs';
+import {  Observable } from 'rxjs';
 import * as articleReducer from '../reducers/article.reducer';
 import * as fromActions from '../actions/article.actions';
-import { ArticleState } from '../reducers/app.states';
 import { Article, FAVORITE_ARTICLES } from '../models/article';
-import { ThrowStmt } from '@angular/compiler';
+
 @Component({
   selector: 'app-article',
   templateUrl: './article.component.html',
